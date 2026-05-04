422ac3a456da6cffd5cc33347689d403b9f91212
ruilovaalex <waruilova@sudamericano.edu.ec>
2026-05-04 13:46:51 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace RuilovaTA_CALIFICACIONES_;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        // Configuramos la ventana inicial para que sea nuestra página de Login
+        return new Window(new LoginPage());
+    }
+}
